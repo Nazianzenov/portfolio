e95d8153d5f4b3894a4a7313fbd1626dc4083fba
Nazianzenov <v.nlejamtel@gmail.com>
2026-01-30 20:39:12 +0100

    production-ready portfolio

---
+++ src/config.ts
@@ -0,0 +1,23 @@
+export const SITE = {
+  website: "https://victorlejamtel.com/", // replace this with your deployed domain
+  author: "Victor Le Jamtel",
+  profile: "https://victorlejamtel.com/",
+  desc: "Building the future of Cyber Threat Intelligence. Security engineering, OSINT, and technical research.",
+  title: "Victor Le Jamtel",
+  ogImage: "",
+  lightAndDarkMode: true,
+  postPerIndex: 4,
+  postPerPage: 6,
+  scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
+  showArchives: true,
+  showBackButton: true,
+  editPost: {
+    enabled: false,
+    text: "Edit page",
+    url: "",
+  },
+  dynamicOgImage: true,
+  dir: "ltr",
+  lang: "en",
+  timezone: "Europe/Paris",
+} as const;
